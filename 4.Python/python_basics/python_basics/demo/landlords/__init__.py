0a535c4fd7d374a5eb92f7d15de8465ac31bc497
小团子 <2625895245@qq.com>
2025-02-02 16:03:53 +0800

    更新扑克游戏

--- 4.Python/python_basics/python_basics/demo/landlords/__init__.py
+++ 4.Python/python_basics/python_basics/demo/landlords/__init__.py
@@ -2,6 +2,7 @@
 斗地主
 '''
 from python_basics.demo.landlords.Board import Board
+import random
 
 if __name__ == '__main__':
     count = 13
@@ -14,8 +15,9 @@ if __name__ == '__main__':
 
     list.append("大王")
     list.append("小王")
-    # print(list)
-
+    print("原始牌",list)
+    random.shuffle(list) # 打乱
+    print("洗牌后",list)
     person01 = {'name':'小张'}
     person02 = {'name': '小李'}
     person03 = {'name': '小韩'}
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     person01['board'] = item01
     person02['board'] = item02
     person03['board'] = item03
-    print(person01)
-    print(person02)
-    print(person03)
+    print(person01['name'],person01['board'])
+    print(person02['name'],person02['board'])
+    print(person03['name'],person03['board'])
     print("底牌",list[-3:])
