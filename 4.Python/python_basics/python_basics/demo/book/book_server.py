d2f032e7ddc497876f2d94a5d13ba371a4fc2658
小团子 <2625895245@qq.com>
2025-02-02 16:06:48 +0800

    更新图书管理系统

--- 4.Python/python_basics/python_basics/demo/book/book_server.py
+++ 4.Python/python_basics/python_basics/demo/book/book_server.py
@@ -10,8 +10,9 @@ def borrow_books(name, list):
             if item[key] == name:
                 if item['state'].__eq__('已借出'):
                     return 2
-                borrow_time = str(input("请输入借阅时间: \n"))
-                item['borrow_time'] = borrow_time
+                return_time = str(input("请输入归还时间: \n"))
+                item['borrow_time'] = date.today()
+                item['return_time'] = return_time
                 item['state'] = '已借出'
                 item['borrow_count'] += 1
                 return 1
@@ -38,9 +39,13 @@ def return_books(name, list):
 删除图书
 """
 def del_books(name, list):
-    for item in list:
-       if item['name'] == name:
-           list.remove(item)
-           return True
-       else:
-           return False
+   for item in list:
+       for key in item:
+           if item[key] == name:
+               list.remove(item)
+               return 1
+           else:
+               # 循环结束，未找到
+               if item.__eq__(list[len(list) - 1]) and item[key] != name:
+                   return 0
+               continue
